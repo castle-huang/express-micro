63fdc10a8607aa41784056039ec4353b3a3de7db
david <you@example.com>
2025-11-13 22:07:07 +0800

    fix

---
+++ apps/auth/src/app.ts
@@ -0,0 +1,14 @@
+//vercel
+import express, {Request, Response} from 'express';
+import {HttpTransport, importAllServices} from "@sojo-micro/rpc";
+
+async function startServer() {
+    await importAllServices();
+    const server = new HttpTransport();
+    const port = parseInt(process.env.PORT || '3001');
+    await server.start(port, ['src', 'apps/auth/src']);
+}
+
+startServer().catch(console.error);
+console.log('Server started');
+

43e5f2159530e8668734e51af3617cd057eb74da
david <you@example.com>
2025-11-16 18:32:34 +0800

    fix

--- apps/analytics/app.ts
+++ apps/analytics/app.ts
@@ -1,32 +1,13 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import {HttpTransport} from "@sojo-micro/rpc";
-import path from "path";
-import fs from "fs";
 
 dotenv.config();
 
 const app = express()
 const server = new HttpTransport(app);
 
-async function importAllServices() {
-    try {
-        const serviceDir = path.join(__dirname, 'src');
-        const files = fs.readdirSync(serviceDir);
-
-        for (const file of files) {
-            if (file.endsWith('.ts') || file.endsWith('.js')) {
-                await import(path.join(serviceDir, file));
-            }
-        }
-    }catch (error){
-        console.log(error)
-    }
-
-}
-
 async function startServer() {
-    await importAllServices()
     await server.scanServices(["src", "apps/analytics/src"])
     const port = parseInt(process.env.PORT || '3000');
     await server.start(port);

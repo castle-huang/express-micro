3ee9c158843cbddcea3954fc518422e4763b9565
david <you@example.com>
2025-11-16 18:45:05 +0800

    fix

--- apps/analytics/app.ts
+++ apps/analytics/app.ts
@@ -11,7 +11,7 @@ const server = new HttpTransport(app);
 
 async function importAllServices(dir: string) {
     try {
-        const serviceDir = path.join(dir, 'src');
+        const serviceDir = path.join(dir, './');
         const files = fs.readdirSync(serviceDir);
 
         for (const file of files) {

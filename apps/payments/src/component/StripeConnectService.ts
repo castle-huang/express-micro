424bcb1c6fa2fc3a8e843ef9b3eaca75033d2fd2
johnwilson3678 <johnwilson3678@gmail.com>
2025-12-17 22:59:48 +0800

    Update StripeConnectService.ts

--- apps/payments/src/component/StripeConnectService.ts
+++ apps/payments/src/component/StripeConnectService.ts
@@ -18,7 +18,7 @@ export class StripeConnectService {
     constructor() {
         this.config = getStripeConnectConfig();
         this.stripe = new Stripe(this.config.secretKey, {
-            apiVersion: '2025-11-17.clover',
+            apiVersion: '2025-12-15.clover',
             typescript: true,
         });
     }
